1c6e8249ae63c43a650b32f652f74b797009e4e7
Nplite <76342ns@gmail.com>
2022-11-16 22:42:11 +0530

    setup completed

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "FSDS_NOV_deepCNNClassifier"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "CNN_DEEP_Learning Classifier"
+AUTHOR_USER_NAME = "Nplite"
 SRC_REPO = "deepClassifier"
-AUTHOR_EMAIL = "sunny.c17hawke@gmail.com"
+AUTHOR_EMAIL = "76342ns@gmail.com"
 
 setuptools.setup(
     name=SRC_REPO,
